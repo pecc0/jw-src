83f282068062ad4993841cada0c67fd86c108832
petkodp <petkodp@b00f97f9-4d11-03a6-6959-139d95cb2541>
2010-10-14 13:14:05 +0000

    DB pass set in properties

--- build-scripts/scripts/BuildRevEng.groovy
+++ build-scripts/scripts/BuildRevEng.groovy
@@ -1,4 +1,5 @@
 import org.apache.tools.ant.Project;
+import org.apache.tools.ant.types.PropertySet;
 import org.hibernate.tool.ant.Hbm2JavaExporterTask;
 import org.hibernate.tool.ant.HibernateToolTask;
 import org.hibernate.tool.ant.JDBCConfigurationTask;
@@ -8,14 +9,37 @@ import groovy.util.ConfigObject;
 
 class BuildRevEng {
 	public void run(ConfigObject config, main) {
-		this.class.classLoader.addClasspath(config.scriptsRoot);
-		//main.addCP(this.class.classLoader.parent, config.scriptsRoot, ~/.*(\.jar|\.properties)/);
+		this.class.classLoader.addClasspath(config.scriptsRoot); //for log4j.properties
+		
+		if (!config.flatten().containsKey('hibernate.connection.password')) {
+			println "Please, add your svnUserName and hibernate.connection.password in local.build.conf: \n" +
+					"  hibernate.connection.password='the pass'\n"
+			throw new RuntimeException("No hibernate.connection.password properties");
+		}
+		
+		PropertySet ps = new PropertySet() {
+			public java.util.Properties getProperties() {
+				return config.toProperties();
+			};
+		}
+		
 		Project project = new Project();
 		HibernateToolTask htt = new HibernateToolTask();
-		JDBCConfigurationTask jdbcConf = htt.createJDBCConfiguration();
+		
+		JDBCConfigurationTask jdbcConf = new JDBCConfigurationTask() {
+			protected java.util.Properties getProperties() {
+				return config.toProperties();
+			};		
+		}
 		jdbcConf.setConfigurationFile(new File('hibernate/hibernate.cfg.xml'));
+		
+		//I'm so glad that I use groovy
+		htt.configurationTask = jdbcConf;
+		
 		Hbm2JavaExporterTask hbm2Java = htt.createHbm2Java();
 		hbm2Java.setEjb3(true);
+		
+		hbm2Java.addConfiguredPropertySet(ps)
 		htt.setDestDir(new File('hibernate/generated'));
 		htt.setProject project;
 		
