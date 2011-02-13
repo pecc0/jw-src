8e4399c962f40904fb94c00e7f4a60aaef04949c
petkodp <petkodp@b00f97f9-4d11-03a6-6959-139d95cb2541>
2011-02-13 12:19:08 +0000

    Fixed url and headers

--- earth/trunk/usr_src/GoogleTilesLoader.cpp
+++ earth/trunk/usr_src/GoogleTilesLoader.cpp
@@ -36,35 +36,43 @@ CImg<pixelFormat> *GoogleTilesLoader::loadTile(int x, int y, int z)
 	coordinatesStr << "x=" << x << "&y=" << y << "&z=" << z;
 
 	std::ostringstream cacheFileStream;
-	cacheFileStream << m_sTmpDir << "\\" << coordinatesStr.str();
+	cacheFileStream << m_sTmpDir << "\\" << coordinatesStr.str()<<".jpg";
 
 	string cacheFileStr(cacheFileStream.str());
 
 	const char* filename = cacheFileStr.c_str();
-	ifstream cacheFile(filename);
+	ifstream cacheStream(filename);
 
-	if (!cacheFile)
+	if (!cacheStream)
 	{
 		//download
 		std::ostringstream url;
 		//TODO url in parameter
-		url << "http://khm0.google.com/kh/v=78&" << coordinatesStr
-				<< "&s=Galileo.jpg";
+		url << "http://khm0.google.com/kh/v=78&" << coordinatesStr.str() << "&s=Galileo.jpg";
 
 		CURL *curl;
 		CURLcode res;
 		curl = curl_easy_init();
 		if (curl)
 		{
+			const char* urlString = url.str().c_str();
+
+			cout<<"URL="<<urlString<<endl;
+
 			struct curl_slist *chunk = NULL;
 
 			chunk = curl_slist_append(chunk, "User-Agent: 	Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13");
+			chunk = curl_slist_append(chunk, "Accept	text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
 
-			curl_easy_setopt(curl, CURLOPT_URL, url.str().c_str());
+			curl_easy_setopt(curl, CURLOPT_URL, urlString);
 			curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+			FILE* cacheFile = fopen(filename, "w+");
+
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, cacheFile);
 
 			res = curl_easy_perform(curl);
 
+			fclose(cacheFile);
 			/* always cleanup */
 			curl_easy_cleanup(curl);
 		}
